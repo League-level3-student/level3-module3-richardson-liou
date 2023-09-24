24bfca311499e1efc39c4a7d3c6e15ae6d5ff24d
richardson-liou <57720048+richardson-liou@users.noreply.github.com>
2023-09-24 12:56:06 -0700

    regex

--- src/_03_Intro_To_Regular_Expressions/_01_RegularExpressionsIntro.java
+++ src/_03_Intro_To_Regular_Expressions/_01_RegularExpressionsIntro.java
@@ -17,6 +17,8 @@ public class _01_RegularExpressionsIntro {
      * Code method below! 
      */
     String emailRemover(String message) {
-        return null;
+    	String originalMessage = message;
+        String modifiedMessage = originalMessage.replaceAll("@\\w+\\.com", "@mail.com");
+        return modifiedMessage;
     }
 }
